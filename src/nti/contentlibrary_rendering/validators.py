8ec455b9f20e6f96e05d50e10c2d6fc9c509797c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-06 23:31:53 +0000

    svn sucks
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentlibrary_rendering/trunk@105943 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentlibrary_rendering/validators.py
+++ src/nti/contentlibrary_rendering/validators.py
@@ -9,16 +9,6 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-import sys
-
-from docutils import frontend
-
-from docutils import nodes
-
-from docutils.parsers.rst import Parser
-
-from docutils.utils import new_reporter
-
 from zope import interface
 
 from nti.contentlibrary.interfaces import IContentValidator
@@ -42,28 +32,3 @@ class TextValidator(object):
     def validate(self, content=b'', context=None):
         self._do_validate(content)
 
-
-@interface.implementer(IContentValidator)
-class ReStructuredTextValidator(object):
-
-    settings = frontend.OptionParser(
-        components=(Parser,)).get_default_values()
-
-    def _do_validate(self, content, context=None):
-        try:
-            parser = Parser()  # XXX: NTI directives should be included
-            reporter = new_reporter("contents", self.settings)
-            document = nodes.document(self.settings,
-                                      reporter,
-                                      source='contents')
-            parser.parse(content, document)
-        except Exception as e:
-            exct = ValidationError("Invalid reStructuredText", e)
-            exct.exc_info = sys.exc_info()
-            raise exct
-
-    def validate(self, content=b'', context=None):
-        if content:
-            self._do_validate(content)
-        else:
-            raise EmptyContentError("Empty content")
