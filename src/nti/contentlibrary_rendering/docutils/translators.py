a9e81ed58d131b6f3e59ba39b0421238b6d6a250
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-16 00:38:49 +0000

    pep8; add mixin
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentlibrary_rendering/trunk@106701 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentlibrary_rendering/docutils/translators.py
+++ src/nti/contentlibrary_rendering/docutils/translators.py
@@ -22,64 +22,67 @@ from nti.contentlibrary_rendering.docutils.interfaces import IRSTToPlastexNodeTr
 
 from nti.contentlibrary_rendering.interfaces import IPlastexDocumentGenerator
 
-@interface.implementer(IRSTToPlastexNodeTranslator)
-class DefaultRSTToPlastexNodeTranslator(object):
-
-    def translate(self, rst_node, tex_doc):
-        result = tex_doc.createElement(rst_node.tagname)
-        return result
 
 @interface.implementer(IRSTToPlastexNodeTranslator)
-class TextRSTToPlastexNodeTranslator(object):
-
-    def translate(self, rst_node, tex_doc):
-        result = tex_doc.createTextNode(rst_node.astext())
-        return result
-
-@interface.implementer(IRSTToPlastexNodeTranslator)
-class TitleRSTToPlastexNodeTranslator(object):
-
-    def translate(self, rst_node, tex_doc):
-        result = tex_doc.createElement(rst_node.tagname)
-        title_text = tex_doc.createTextNode(rst_node.astext())
-        result.append( title_text )
-        return result
-
-class _NoopRSTToPlastexNodeTranslator(object):
+class TranslatorMixin(object):
     """
     A translator that excludes this node from translation.
     """
 
     def translate(self, rst_node, tex_doc):
         pass
+_NoopRSTToPlastexNodeTranslator = TranslatorMixin
 
-@interface.implementer(IRSTToPlastexNodeTranslator)
-class ImageRSTToPlastexNodeTranslator(_NoopRSTToPlastexNodeTranslator):
+
+class DefaultRSTToPlastexNodeTranslator(TranslatorMixin):
+
+    def translate(self, rst_node, tex_doc):
+        result = tex_doc.createElement(rst_node.tagname)
+        return result
+
+
+class TextRSTToPlastexNodeTranslator(TranslatorMixin):
+
+    def translate(self, rst_node, tex_doc):
+        result = tex_doc.createTextNode(rst_node.astext())
+        return result
+
+
+class TitleRSTToPlastexNodeTranslator(TranslatorMixin):
+
+    def translate(self, rst_node, tex_doc):
+        result = tex_doc.createElement(rst_node.tagname)
+        title_text = tex_doc.createTextNode(rst_node.astext())
+        result.append(title_text)
+        return result
+
+
+class ImageRSTToPlastexNodeTranslator(TranslatorMixin):
     pass
 
-@interface.implementer(IRSTToPlastexNodeTranslator)
-class MathRSTToPlastexNodeTranslator(_NoopRSTToPlastexNodeTranslator):
+
+class MathRSTToPlastexNodeTranslator(TranslatorMixin):
     pass
 
-@interface.implementer(IRSTToPlastexNodeTranslator)
-class SectionRSTToPlastexNodeTranslator(_NoopRSTToPlastexNodeTranslator):
+
+class SectionRSTToPlastexNodeTranslator(TranslatorMixin):
     # XXX: if we include sections, we'll need title attributes.
     pass
 
-@interface.implementer(IRSTToPlastexNodeTranslator)
-class ParagraphRSTToPlastexNodeTranslator(object):
+
+class ParagraphRSTToPlastexNodeTranslator(TranslatorMixin):
 
     def translate(self, unused_rst_node, tex_doc):
         result = tex_doc.createElement('par')
         return result
 
-@interface.implementer(IRSTToPlastexNodeTranslator)
-class SubtitleRSTToPlastexNodeTranslator(object):
+
+class SubtitleRSTToPlastexNodeTranslator(TranslatorMixin):
 
     def translate(self, rst_node, tex_doc):
         # XXX: Do we want a new section here?
         result = tex_doc.createElement('section')
-        names = rst_node.attributes.get( 'names' )
+        names = rst_node.attributes.get('names')
         if names:
             title = names[0]
         else:
@@ -88,8 +91,8 @@ class SubtitleRSTToPlastexNodeTranslator(object):
         result.setAttribute('title', title)
         return result
 
-@interface.implementer(IRSTToPlastexNodeTranslator)
-class DocumentRSTToPlastexNodeTranslator(object):
+
+class DocumentRSTToPlastexNodeTranslator(TranslatorMixin):
 
     def translate(self, rst_node, tex_doc):
         result = tex_doc.createElement(rst_node.tagname)
@@ -98,6 +101,7 @@ class DocumentRSTToPlastexNodeTranslator(object):
         # The document root (and sections?) will need a title element.
         result.setAttribute('title', title)
         return result
+
 
 @interface.implementer(IPlastexDocumentGenerator)
 class RSTToPlastexDocumentGenerator(object):
@@ -109,7 +113,7 @@ class RSTToPlastexDocumentGenerator(object):
         return translator(node_name)
 
     def _handle_node(self, rst_node, tex_parent, tex_doc):
-        node_translator = self._get_node_translator( rst_node.tagname )
+        node_translator = self._get_node_translator(rst_node.tagname)
         tex_node = node_translator.translate(rst_node, tex_doc)
         if tex_node is not None:
             tex_parent.append(tex_node)
@@ -124,7 +128,7 @@ class RSTToPlastexDocumentGenerator(object):
         for rst_child in rst_parent.children or ():
             self.build_nodes(rst_child, tex_node, tex_doc)
 
-    def generate(self, rst_document, tex_doc=None):
+    def generate(self, rst_document=None, tex_doc=None):
         # XXX: By default, we skip any preamble and start directly in the
         # body. docutils stores the title info in the preamble.
         document = TeXDocument() if tex_doc is None else tex_doc
