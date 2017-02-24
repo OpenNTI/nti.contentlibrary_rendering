df211e51e3fc9ec475d3358572e8cd69c00147d4
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-24 20:07:46 +0000

    test roles
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentlibrary_rendering/trunk@107417 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentlibrary_rendering/docutils/tests/test_roles.py
+++ src/nti/contentlibrary_rendering/docutils/tests/test_roles.py
@@ -7,10 +7,13 @@ __docformat__ = "restructuredtext en"
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
-
+from hamcrest import none
+from hamcrest import is_not
 from hamcrest import assert_that
 
 from nti.testing.matchers import validly_provides
+
+from docutils.parsers.rst import languages
 
 from nti.contentlibrary_rendering.docutils.interfaces import IRolesModule
 
@@ -21,4 +24,9 @@ class TestRoles(ContentlibraryRenderingLayerTest):
 
     def test_roles(self):
         from nti.contentlibrary_rendering.docutils import roles
-        assert_that(roles, validly_provides(IRolesModule))
+        assert_that(roles, validly_provides(IRolesModule)) 
+        assert_that(languages.en.roles['underlined'], is_not(none()))
+        assert_that(languages.en.roles['bolditalic'], is_not(none()))
+        assert_that(languages.en.roles['boldunderlined'], is_not(none()))
+        assert_that(languages.en.roles['italicunderlined'], is_not(none()))
+        assert_that(languages.en.roles['bolditalicunderlined'], is_not(none()))
