76a2ad422752d5b43ebc9cf66b6f17b943e18052
Josh Zuech <josh.zuech@nextthought.com>
2017-02-24 05:58:45 +0000

    Enable locator
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentlibrary_rendering/trunk@107371 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentlibrary_rendering/utils.py
+++ src/nti/contentlibrary_rendering/utils.py
@@ -60,7 +60,7 @@ def remove_renderered_package(package):
     assert IRenderableContentPackage.providedBy(package)
     site_name = find_interface(package, IHostPolicyFolder).__name__
     job_id="remove_renderered_content_%s" % package.ntiid
-    put_job(CONTENT_UNITS_QUEUE, 
+    put_job(CONTENT_UNITS_QUEUE,
             remove_rendered_package,
             job_id=job_id,
             ntiid=package.ntiid,
