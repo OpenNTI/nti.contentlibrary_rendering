b0cc1aabfb86aa18d6c8b0bf8444882d3306936c
Josh Zuech <josh.zuech@nextthought.com>
2017-03-03 16:52:36 +0000

    Make sure dir has executable perms for all
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentlibrary_rendering/trunk@107918 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentlibrary_rendering/locators.py
+++ src/nti/contentlibrary_rendering/locators.py
@@ -97,7 +97,7 @@ class FilesystemLocator(LocatorMixin):
         os.chmod(file_,
                  stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR |
                  stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP |
-                 stat.S_IROTH)
+                 stat.S_IROTH | stat.S_IXOTH )
 
     def _do_locate(self, path, root, context):
         assert isinstance(root, FilesystemBucket)
