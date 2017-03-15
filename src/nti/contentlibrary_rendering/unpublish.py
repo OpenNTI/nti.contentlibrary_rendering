bae50725a8f28897bc311a8b840e978369bfdd92
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-15 04:02:11 +0000

    remove assert
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentlibrary_rendering/trunk@108897 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentlibrary_rendering/unpublish.py
+++ src/nti/contentlibrary_rendering/unpublish.py
@@ -11,8 +11,6 @@ logger = __import__('logging').getLogger(__name__)
 
 from zope import component
 
-from nti.contentlibrary.interfaces import IRenderableContentPackage
-
 from nti.contentlibrary_rendering.interfaces import IRenderedContentLocator
 
 from nti.contentlibrary_rendering import CONTENT_UNITS_QUEUE
@@ -30,7 +28,6 @@ def remove_rendered_package(bucket):
 
 
 def queue_remove_rendered_package(ntiid, root, site_name=None):
-    assert IRenderableContentPackage.providedBy(package)
     # Have to pass the bucket to the package remove function since
     # the package will no longer be resolvable outside this transaction.
     # This must be enough info to cleanup whatever needs to be cleaned up.
