e3d5ac2692c0d585cbdd9463f329e9123619b265
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-16 21:06:30 +0000

    add test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentlibrary_rendering/trunk@109018 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contentlibrary_rendering/docutils/tests/test_model.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+
+from hamcrest import is_
+from hamcrest import has_length
+from hamcrest import assert_that
+from hamcrest import has_entries
+
+import os
+
+from nti.contentlibrary.zodb import RenderableContentPackage
+
+from nti.contentlibrary_rendering.tests import ContentlibraryRenderingLayerTest
+
+from nti.externalization.externalization import to_external_object
+
+
+class TestModel(ContentlibraryRenderingLayerTest):
+
+    def _get_data(self, source):
+        name = os.path.join(os.path.dirname(__file__), 'data/%s' % source)
+        with open(name, "rb") as fp:
+            return fp.read()
+
+    def test_renderable_io(self):
+        package = RenderableContentPackage()
+        package.ntiid = u'tag:nextthought.com,2011-10:NTI-HTML-human_physiology'
+        package.title = u'Human Physiology'
+        package.description = u'Human Physiology'
+        package.contentType = b'text/x-rst'
+        package.contents = self._get_data("label.rst")
+        ext_obj = to_external_object(package, name='exporter')
+        assert_that(ext_obj,
+                    has_entries(u'isPublished', is_(False),
+                                u'MimeType', 'application/vnd.nextthought.renderablecontentpackage',
+                                u'NTIID', u'tag:nextthought.com,2011-10:NTI-HTML-human_physiology',
+                                u'title', is_(u'Human Physiology'),
+                                u'contents', has_length(262)))
