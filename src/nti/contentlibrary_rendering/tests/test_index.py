5c11b25f20747e8088fca4db354d24266453b58d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-06-03 14:16:56 +0000

    test query
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentlibrary_rendering/trunk@114143 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentlibrary_rendering/tests/test_index.py
+++ src/nti/contentlibrary_rendering/tests/test_index.py
@@ -13,6 +13,8 @@ from hamcrest import is_not
 from hamcrest import has_length
 from hamcrest import assert_that
 
+from nti.testing.matchers import is_empty
+
 from nti.contentlibrary_rendering.interfaces import FAILED
 
 from nti.contentlibrary_rendering.index import ContentRenderJobCatalog
@@ -26,6 +28,7 @@ from nti.contentlibrary_rendering.tests import ContentlibraryRenderingTestLayer
 class TestIndex(ContentlibraryRenderingTestLayer):
 
     def test_catalog(self):
+        ntiid = u'tag:nextthought.com,2011-10:USSC-HTML-Cohen.cohen_v._california'
         catalog = create_contentrenderjob_catalog()
         assert_that(catalog, is_not(none()))
         assert_that(catalog, has_length(8))
@@ -33,8 +36,17 @@ class TestIndex(ContentlibraryRenderingTestLayer):
 
         job = ContentPackageRenderJob()
         job.JobId = u'123456'
-        job.PackageNTIID = u'tag:nextthought.com,2011-10:USSC-HTML-Cohen.cohen_v._california'
+        job.PackageNTIID = ntiid
         job.State = FAILED
         job.creator = u'ichigo'
         job.updateLastMod()
         catalog.index_doc(1, job)
+
+        for query in (
+                {'state': {'any_of': (FAILED,)}},
+                {'jobId': {'any_of': ('123456',)}},
+                {'creator': {'any_of': ('ichigo',)}},
+                {'packageNTIID': {'any_of': (ntiid,)}},
+                {'mimeType': {'any_of': ('application/vnd.nextthought.content.packagerenderjob',)}}):
+            results = catalog.apply(query) or ()
+            assert_that(results, is_not(is_empty()))
