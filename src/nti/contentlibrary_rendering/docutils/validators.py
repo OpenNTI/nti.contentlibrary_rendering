6aed5a3772c51f7d919dfe2c107ad961a3713d57
Josh Zuech <josh.zuech@nextthought.com>
2017-06-13 21:39:16 +0000

    Attempt to clean up up RST validation error message
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentlibrary_rendering/trunk@114976 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentlibrary_rendering/docutils/validators.py
+++ src/nti/contentlibrary_rendering/docutils/validators.py
@@ -9,11 +9,18 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import re
+
 from zope import interface
 
 from nti.contentlibrary.validators import ContentValidationError
 
 from nti.contentlibrary_rendering.docutils.interfaces import IRSTContentValidationError
+
+# We'd like to try to clean up all the boilerplate text.
+# XXX: Do we want to try to retain line number etc?
+# ex: '<string>:2: (SEVERE/4) Unexpected section title or transition.\n\n========'
+MSG_PATTERN = r'<.*\) (.*)'
 
 
 @interface.implementer(IRSTContentValidationError)
@@ -22,5 +29,8 @@ class RSTContentValidationError(ContentValidationError):
     mime_type = mimeType = 'application/vnd.nextthought.content.rstvalidationerror'
 
     def __init__(self, message, warnings=None, *args, **kwargs):
+        match = re.match(r'<.*\) (.*)', message)
+        if match and match.groups():
+            message = match.groups()[0]
         ContentValidationError.__init__(self, message, *args, **kwargs)
         self.warnings = warnings
