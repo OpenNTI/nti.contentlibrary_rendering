31b25564a40c101df9da1c11933bdddf1b65f320
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-11-02 08:45:36 +0000

    add default sphinx conf
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentlibrary_rendering/trunk@123669 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contentlibrary_rendering/docutils/sphinx/conf.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
+# This is also used if you do content translation via gettext catalogs.
+# Usually you set "language" from the command line for these cases.
+language = None
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# The default language to highlight source code in.
+highlight_language = 'python'
