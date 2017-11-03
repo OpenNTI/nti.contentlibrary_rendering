e2e635340fb814d2d27306a4570f752130ffc7de
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-11-03 03:24:43 +0000

    remove unused code
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentlibrary_rendering/trunk@123681 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentlibrary_rendering/docutils/sphinx/__init__.py
+++ src/nti/contentlibrary_rendering/docutils/sphinx/__init__.py
@@ -7,31 +7,3 @@
 from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
-
-import os
-import logging
-import tempfile
-
-try:
-    from sphinx import application
-
-    # reduce verbosity
-    logger = application.logger
-    logger.setLevel(logging.ERROR)
-
-    def create_sphinx(srcdir=None,
-                      confdir=None,
-                      outdir=None,
-                      doctreedir=None,
-                      buildername='html'):
-        # set defaults
-        outdir = outdir or tempfile.gettempdir()
-        srcdir = srcdir or os.path.dirname(__file__)
-        confdir = confdir or os.path.dirname(__file__)
-        doctreedir = doctreedir or os.path.dirname(__file__)
-        # return app
-        return application.Sphinx(srcdir, confdir,
-                                  outdir, doctreedir, buildername)
-except ImportError:
-    def create_sphinx(*args, **kwargs):
-        pass
