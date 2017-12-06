af066e3d78ae61cd2c6e42ca4495d910c68146fb
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-12-06 17:33:34 +0000

    add RST code block error
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentlibrary_rendering/trunk@124589 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentlibrary_rendering/docutils/validators.py
+++ src/nti/contentlibrary_rendering/docutils/validators.py
@@ -19,7 +19,7 @@ from nti.contentlibrary_rendering.docutils.interfaces import IRSTContentValidati
 # We'd like to try to clean up all the boilerplate text.
 # Do we want to try to retain line number etc?
 # ex: '<string>:2: (SEVERE/4) Unexpected section title or transition.\n\n========'
-MSG_PATTERN = r'<.*\) (.*)'
+MSG_PATTERN = r'<.*>.*(\(.*\))\s*(.*)'
 
 logger = __import__('logging').getLogger(__name__)
 
@@ -30,8 +30,13 @@ class RSTContentValidationError(ContentValidationError):
     mime_type = mimeType = 'application/vnd.nextthought.content.rstvalidationerror'
 
     def __init__(self, message, warnings=None, *args, **kwargs):
-        match = re.match(r'<.*\) (.*)', message)
+        match = re.match(MSG_PATTERN, message)
         if match and match.groups():
-            message = match.groups()[0]
+            message = match.groups()[1]
         ContentValidationError.__init__(self, message, *args, **kwargs)
         self.warnings = warnings
+
+
+@interface.implementer(IRSTContentValidationError)
+class RSTCodeBlockError(RSTContentValidationError):
+    mime_type = mimeType = 'application/vnd.nextthought.content.rstcodeblockerror'
