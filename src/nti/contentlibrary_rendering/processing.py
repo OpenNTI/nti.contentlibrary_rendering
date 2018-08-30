d0245a57a2cf70ef9fd37647f71314a46d7dea69
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-08-30 14:40:03 -0500

    pylint

--- src/nti/contentlibrary_rendering/processing.py
+++ src/nti/contentlibrary_rendering/processing.py
@@ -8,6 +8,8 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
+# pylint: disable=keyword-arg-before-vararg
+
 from zope import component
 
 from zope.component.hooks import getSite
